37f3287b3af6e7940405a71af4fda9bb6d578074
Evgeny Im <evgenyim6456@gmail.com>
2021-09-08 15:04:34 +0300

    add expressions evaluation

--- server/src/main/kotlin/com/example/plugins/EvaluationHandler.kt
+++ server/src/main/kotlin/com/example/plugins/EvaluationHandler.kt
@@ -2,7 +2,10 @@ package com.example.plugins
 
 import com.example.models.EvalRequest
 import com.example.models.Expression
+import com.example.models.ExpressionErrorModel
 import com.example.models.ExpressionResultModel
+import java.lang.Exception
+import org.mariuszgromada.math.mxparser.Expression as Expr
 
 object EvaluationHandler {
   /**
@@ -10,7 +13,19 @@ object EvaluationHandler {
    * @return ExpressionResultModel if ho errors occurred and ExpressionErrorModel instead
    */
   fun evaluateExpression(evalRequest: EvalRequest): Expression {
-    //TODO
-    return ExpressionResultModel(evalRequest.str, 0.0)
+    if (!validate(evalRequest.str)) {
+      return ExpressionErrorModel(evalRequest.str, "Wrong string format")
+    }
+    val res: Double
+    try {
+      res = Expr(evalRequest.str).calculate()
+    } catch (e: Exception) {
+      return ExpressionErrorModel(evalRequest.str, "Error in evaluation")
+    }
+    return ExpressionResultModel(evalRequest.str, res)
+  }
+
+  fun validate(expr: String): Boolean {
+    return expr.matches(Regex("[-0-9+*/%.^() ]*"))
   }
 }
